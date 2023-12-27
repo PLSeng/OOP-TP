ab8ec540113a88db6b6d0e81784d12346cb83da5
PAV Limseng <limseng2017@outlook.com>
2023-12-27 14:29:02 +0700

    added TP-09 and TP-10

---
+++ TP-09/src/CheckedException.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class CheckedException {
+    public static void main(String[] args) throws IOException {
+        try {
+            FileReader fileReader = new FileReader("hello.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+        System.out.println("Finished .....");
+
+        try {
+            FileWriter fileWriter = new FileWriter("hello.txt");
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write("Siemreap is a beautiful city.");
+            bufferedWriter.newLine();
+            bufferedWriter.write("Takeo is an old province.");
+            bufferedWriter.close();
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+    }
+}
+
---
+++ TP-09/src/UncheckedException.java
@@ -0,0 +1,12 @@
+public class UncheckedException {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 0;
+
+        try{
+            System.out.println(a/b);
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
